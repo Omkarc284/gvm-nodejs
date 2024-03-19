b0fdf285ff22b185e00eef94db42ce8f563fe5c1
omkarc284 <omkarc284@gmail.com>
2024-03-19 11:50:12 +0530

    final draft

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const dotenv = require('dotenv');
 dotenv.config();
 const bodyParser = require('body-parser');
@@ -22,7 +23,13 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
     next();
 });
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+}));
 app.use(passport.initialize());
+app.use(passport.session());
 app.use('/tasks', tasksRouter);
 app.use('/upload', uploadRouter);
 app.use('/auth', registrationRouter);
